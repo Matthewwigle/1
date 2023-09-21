5475970165862aeabee60eb880ae59ee530257fe
Matthewwigle <144264777+Matthewwigle@users.noreply.github.com>
2023-09-21 08:30:42 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace ITEC140_A02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCompleteTotal = new System.Windows.Forms.Label();
+            this.lblTotalTax = new System.Windows.Forms.Label();
+            this.btnTotalTax = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnTotal = new System.Windows.Forms.Button();
+            this.lstReceipt = new System.Windows.Forms.ListBox();
+            this.btnWater = new System.Windows.Forms.Button();
+            this.lblWater = new System.Windows.Forms.Label();
+            this.btnSauce = new System.Windows.Forms.Button();
+            this.btnPasta = new System.Windows.Forms.Button();
+            this.lblPasta = new System.Windows.Forms.Label();
+            this.btnSandwich = new System.Windows.Forms.Button();
+            this.lblSandwich = new System.Windows.Forms.Label();
+            this.btnLatte = new System.Windows.Forms.Button();
+            this.lblLatte = new System.Windows.Forms.Label();
+            this.btnCoffee = new System.Windows.Forms.Button();
+            this.lblCoffee = new System.Windows.Forms.Label();
+            this.btnSalad = new System.Windows.Forms.Button();
+            this.lblSalad = new System.Windows.Forms.Label();
+            this.btnPizza = new System.Windows.Forms.Button();
+            this.lblPizza = new System.Windows.Forms.Label();
+            this.lblSauce = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCompleteTotal
+            // 
+            this.lblCompleteTotal.AutoSize = true;
+            this.lblCompleteTotal.Location = new System.Drawing.Point(486, 423);
+            this.lblCompleteTotal.Name = "lblCompleteTotal";
+            this.lblCompleteTotal.Size = new System.Drawing.Size(34, 13);
+            this.lblCompleteTotal.TabIndex = 0;
+            this.lblCompleteTotal.Text = "$0.00";
+            // 
+            // lblTotalTax
+            // 
+            this.lblTotalTax.AutoSize = true;
+            this.lblTotalTax.Location = new System.Drawing.Point(486, 383);
+            this.lblTotalTax.Name = "lblTotalTax";
+            this.lblTotalTax.Size = new System.Drawing.Size(34, 13);
+            this.lblTotalTax.TabIndex = 1;
+            this.lblTotalTax.Text = "$0.00";
+            this.lblTotalTax.Click += new System.EventHandler(this.lblTotalTax_Click);
+            // 
+            // btnTotalTax
+            // 
+            this.btnTotalTax.Location = new System.Drawing.Point(405, 378);
+            this.btnTotalTax.Name = "btnTotalTax";
+            this.btnTotalTax.Size = new System.Drawing.Size(75, 23);
+            this.btnTotalTax.TabIndex = 2;
+            this.btnTotalTax.Text = "Total Tax";
+            this.btnTotalTax.UseVisualStyleBackColor = true;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(486, 319);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(34, 13);
+            this.lblTotal.TabIndex = 3;
+            this.lblTotal.Text = "$0.00";
+            this.lblTotal.Click += new System.EventHandler(this.lblTotal_Click);
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.Location = new System.Drawing.Point(405, 314);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(75, 23);
+            this.btnTotal.TabIndex = 4;
+            this.btnTotal.Text = "Total";
+            this.btnTotal.UseVisualStyleBackColor = true;
+            this.btnTotal.Click += new System.EventHandler(this.btnTotal_Click);
+            // 
+            // lstReceipt
+            // 
+            this.lstReceipt.FormattingEnabled = true;
+            this.lstReceipt.Location = new System.Drawing.Point(526, 6);
+            this.lstReceipt.Name = "lstReceipt";
+            this.lstReceipt.Size = new System.Drawing.Size(291, 368);
+            this.lstReceipt.TabIndex = 5;
+            // 
+            // btnWater
+            // 
+            this.btnWater.Location = new System.Drawing.Point(246, 207);
+            this.btnWater.Name = "btnWater";
+            this.btnWater.Size = new System.Drawing.Size(75, 23);
+            this.btnWater.TabIndex = 6;
+            this.btnWater.Text = "$1.10";
+            this.btnWater.UseVisualStyleBackColor = true;
+            // 
+            // lblWater
+            // 
+            this.lblWater.AutoSize = true;
+            this.lblWater.Location = new System.Drawing.Point(246, 191);
+            this.lblWater.Name = "lblWater";
+            this.lblWater.Size = new System.Drawing.Size(36, 13);
+            this.lblWater.TabIndex = 7;
+            this.lblWater.Text = "Water";
+            // 
+            // btnSauce
+            // 
+            this.btnSauce.Location = new System.Drawing.Point(12, 207);
+            this.btnSauce.Name = "btnSauce";
+            this.btnSauce.Size = new System.Drawing.Size(75, 23);
+            this.btnSauce.TabIndex = 8;
+            this.btnSauce.Text = "$0.50";
+            this.btnSauce.UseVisualStyleBackColor = true;
+            this.btnSauce.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnPasta
+            // 
+            this.btnPasta.Location = new System.Drawing.Point(246, 150);
+            this.btnPasta.Name = "btnPasta";
+            this.btnPasta.Size = new System.Drawing.Size(75, 23);
+            this.btnPasta.TabIndex = 9;
+            this.btnPasta.Text = "$5.49";
+            this.btnPasta.UseVisualStyleBackColor = true;
+            // 
+            // lblPasta
+            // 
+            this.lblPasta.AutoSize = true;
+            this.lblPasta.Location = new System.Drawing.Point(243, 134);
+            this.lblPasta.Name = "lblPasta";
+            this.lblPasta.Size = new System.Drawing.Size(34, 13);
+            this.lblPasta.TabIndex = 10;
+            this.lblPasta.Text = "Pasta";
+            // 
+            // btnSandwich
+            // 
+            this.btnSandwich.Location = new System.Drawing.Point(12, 150);
+            this.btnSandwich.Name = "btnSandwich";
+            this.btnSandwich.Size = new System.Drawing.Size(75, 23);
+            this.btnSandwich.TabIndex = 11;
+            this.btnSandwich.Text = "$10.00";
+            this.btnSandwich.UseVisualStyleBackColor = true;
+            // 
+            // lblSandwich
+            // 
+            this.lblSandwich.AutoSize = true;
+            this.lblSandwich.Location = new System.Drawing.Point(12, 134);
+            this.lblSandwich.Name = "lblSandwich";
+            this.lblSandwich.Size = new System.Drawing.Size(54, 13);
+            this.lblSandwich.TabIndex = 12;
+            this.lblSandwich.Text = "Sandwich";
+            // 
+            // btnLatte
+            // 
+            this.btnLatte.Location = new System.Drawing.Point(246, 89);
+            this.btnLatte.Name = "btnLatte";
+            this.btnLatte.Size = new System.Drawing.Size(75, 23);
+            this.btnLatte.TabIndex = 13;
+            this.btnLatte.Text = "$5.99";
+            this.btnLatte.UseVisualStyleBackColor = true;
+            // 
+            // lblLatte
+            // 
+            this.lblLatte.AutoSize = true;
+            this.lblLatte.Location = new System.Drawing.Point(246, 73);
+            this.lblLatte.Name = "lblLatte";
+            this.lblLatte.Size = new System.Drawing.Size(31, 13);
+            this.lblLatte.TabIndex = 14;
+            this.lblLatte.Text = "Latte";
+            this.lblLatte.Click += new System.EventHandler(this.lblLatte_Click);
+            // 
+            // btnCoffee
+            // 
+            this.btnCoffee.Location = new System.Drawing.Point(12, 89);
+            this.btnCoffee.Name = "btnCoffee";
+            this.btnCoffee.Size = new System.Drawing.Size(75, 23);
+            this.btnCoffee.TabIndex = 15;
+            this.btnCoffee.Text = "$1.99";
+            this.btnCoffee.UseVisualStyleBackColor = true;
+            // 
+            // lblCoffee
+            // 
+            this.lblCoffee.AutoSize = true;
+            this.lblCoffee.Location = new System.Drawing.Point(12, 73);
+            this.lblCoffee.Name = "lblCoffee";
+            this.lblCoffee.Size = new System.Drawing.Size(38, 13);
+            this.lblCoffee.TabIndex = 16;
+            this.lblCoffee.Text = "Coffee";
+            // 
+            // btnSalad
+            // 
+            this.btnSalad.Location = new System.Drawing.Point(246, 39);
+            this.btnSalad.Name = "btnSalad";
+            this.btnSalad.Size = new System.Drawing.Size(83, 23);
+            this.btnSalad.TabIndex = 17;
+            this.btnSalad.Text = "\t$2.99";
+            this.btnSalad.UseVisualStyleBackColor = true;
+            // 
+            // lblSalad
+            // 
+            this.lblSalad.AutoSize = true;
+            this.lblSalad.Location = new System.Drawing.Point(246, 23);
+            this.lblSalad.Name = "lblSalad";
+            this.lblSalad.Size = new System.Drawing.Size(34, 13);
+            this.lblSalad.TabIndex = 18;
+            this.lblSalad.Text = "Salad";
+            // 
+            // btnPizza
+            // 
+            this.btnPizza.Location = new System.Drawing.Point(12, 39);
+            this.btnPizza.Name = "btnPizza";
+            this.btnPizza.Size = new System.Drawing.Size(75, 23);
+            this.btnPizza.TabIndex = 19;
+            this.btnPizza.Text = "$1.99";
+            this.btnPizza.UseVisualStyleBackColor = true;
+            this.btnPizza.Click += new System.EventHandler(this.btnPizza_Click);
+            // 
+            // lblPizza
+            // 
+            this.lblPizza.AutoSize = true;
+            this.lblPizza.Location = new System.Drawing.Point(12, 23);
+            this.lblPizza.Name = "lblPizza";
+            this.lblPizza.Size = new System.Drawing.Size(32, 13);
+            this.lblPizza.TabIndex = 20;
+            this.lblPizza.Text = "Pizza";
+            // 
+            // lblSauce
+            // 
+            this.lblSauce.AutoSize = true;
+            this.lblSauce.Location = new System.Drawing.Point(15, 188);
+            this.lblSauce.Name = "lblSauce";
+            this.lblSauce.Size = new System.Drawing.Size(38, 13);
+            this.lblSauce.TabIndex = 21;
+            this.lblSauce.Text = "Sauce";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 449);
+            this.Controls.Add(this.lblSauce);
+            this.Controls.Add(this.lblPizza);
+            this.Controls.Add(this.btnPizza);
+            this.Controls.Add(this.lblSalad);
+            this.Controls.Add(this.btnSalad);
+            this.Controls.Add(this.lblCoffee);
+            this.Controls.Add(this.btnCoffee);
+            this.Controls.Add(this.lblLatte);
+            this.Controls.Add(this.btnLatte);
+            this.Controls.Add(this.lblSandwich);
+            this.Controls.Add(this.btnSandwich);
+            this.Controls.Add(this.lblPasta);
+            this.Controls.Add(this.btnPasta);
+            this.Controls.Add(this.btnSauce);
+            this.Controls.Add(this.lblWater);
+            this.Controls.Add(this.btnWater);
+            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnTotalTax);
+            this.Controls.Add(this.lblTotalTax);
+            this.Controls.Add(this.lblCompleteTotal);
+            this.Controls.Add(this.lstReceipt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCompleteTotal;
+        private System.Windows.Forms.Label lblTotalTax;
+        private System.Windows.Forms.Button btnTotalTax;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnTotal;
+        private System.Windows.Forms.ListBox lstReceipt;
+        private System.Windows.Forms.Button btnWater;
+        private System.Windows.Forms.Label lblWater;
+        private System.Windows.Forms.Button btnSauce;
+        private System.Windows.Forms.Button btnPasta;
+        private System.Windows.Forms.Label lblPasta;
+        private System.Windows.Forms.Button btnSandwich;
+        private System.Windows.Forms.Label lblSandwich;
+        private System.Windows.Forms.Button btnLatte;
+        private System.Windows.Forms.Label lblLatte;
+        private System.Windows.Forms.Button btnCoffee;
+        private System.Windows.Forms.Label lblCoffee;
+        private System.Windows.Forms.Button btnSalad;
+        private System.Windows.Forms.Label lblSalad;
+        private System.Windows.Forms.Button btnPizza;
+        private System.Windows.Forms.Label lblPizza;
+        private System.Windows.Forms.Label lblSauce;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITEC140_A02
+{
+    [DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+    public partial class Form1 : Form
+    {
+        int decTax = 0;
+        int decTotalWithTax = 0;
+        int decTotal = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblLatte_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTotalTax_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPizza_Click(object sender, EventArgs e)
+        {
+            decTotal = decTotal = decimal = 1.99m;
+            btnTotal = btnTotal.ToString();
+        }
+
+        private void lblTotal_Click(object sender, EventArgs e)
+        {
+            lblTotal=decTotal.ToString();
+        }
+
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+   
